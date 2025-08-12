923d714c469778f8585e69295a7ac7c24f3f2a98
Yaniv Sharon <ys.yaniv.sharon@gmail.com>
2025-08-12 17:00:45 +0200

    Implemented getting items from the db and added a few

--- lib/persist-module.ts
+++ lib/persist-module.ts
@@ -1,8 +1,19 @@
 "use server";
 
-import { shop_items } from "./shop-item-data"; //TODO: Move the contents of that file here
 import sqlite from "better-sqlite3";
 
+const db = await openDb();
+
+// Open SQLite database connection
+async function openDb() {
+  const options = {
+    fileMustExist: true,
+    // verbose: console.log
+  };
+  return sqlite("./database.sqlite3", options);
+}
+
+// Authentication
 export enum AuthLevel {
   None,
   Normal,
@@ -15,39 +26,40 @@ export type User = {
   authLevel: AuthLevel;
 };
 
-// Open SQLite database connection
-async function openDb() {
-  const options = {
-    fileMustExist: true,
-    // verbose: console.log
-  };
-  return sqlite("./database.sqlite3", options);
-}
-
 export async function addUser(
   email: string,
   hashedPassword: string,
   authLevel: number,
 ) {
-  let db = await openDb();
-
   const insertUser = db.prepare(
     "INSERT INTO Users (email, password, authLevel) VALUES (?, ?, ?)",
   );
   insertUser.run(email, hashedPassword, authLevel);
 
-  db.close();
-
   return getUser(email);
 }
 
 export async function getUser(email: string) {
-  let db = await openDb();
-
   const getUser = db.prepare("SELECT * FROM Users WHERE email = ?");
   const user = getUser.get(email) as User;
 
-  db.close();
-
   return user;
+}
+
+// Shop items
+export type ShopItem = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+export async function getShopItem(id: number) {
+  const getShopItem = db.prepare("SELECT * FROM Items WHERE id = ?");
+  const shopItem = getShopItem.get(id) as ShopItem;
+
+  return shopItem;
+}
+
+export async function addShopItem(item: ShopItem) {
+  // TODO: Implement
 }
