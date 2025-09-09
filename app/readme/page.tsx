7532accd2accdbd974e66b96e5de62fc77103213
Yaniv Sharon <ys.yaniv.sharon@gmail.com>
2025-09-09 20:53:23 +0200

    Readme (#21)
    * Sort of made a tests file
    
    * Added a readme folder/route

---
+++ app/readme/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <Link href="/readme/yaniv">Yaniv's readme</Link>
+      <br />
+      <Link href="/readme/natalie">Natalie's readme</Link>
+    </div>
+  );
+}
