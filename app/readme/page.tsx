5d9fc69fa2a69a5d79539a1cac0605b402a698b1
Yaniv Sharon <ys.yaniv.sharon@gmail.com>
2025-09-11 14:03:20 +0300

    Fixed readme page

--- app/readme/page.tsx
+++ app/readme/page.tsx
@@ -1,13 +1,5 @@
 "use client";
 
-import Link from "next/link";
-
 export default function Page() {
-  return (
-    <div>
-      <Link href="/readme/yaniv">Yaniv's readme</Link>
-      <br />
-      <Link href="/readme/natalie">Natalie's readme</Link>
-    </div>
-  );
+  return <div></div>;
 }
